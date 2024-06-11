88fb04ced7d24e2147cd9ad9d406f3583f9fe717
Zeha <zehakhan33151@gmail.com>
2024-06-11 09:42:47 +0530

    Fix bug in data processing function

--- data_visualization.py
+++ data_visualization.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import csv
 
 def visualize_data():
     data = []
@@ -7,7 +8,11 @@ def visualize_data():
         for row in reader:
             data.append(float(row[0]))
     plt.plot(data)
+    plt.xlabel('Time')
+    plt.ylabel('Temperature (°C)')
+    plt.title('Temperature Over Time')
     plt.show()
 
 # Example test
 visualize_data()
+
